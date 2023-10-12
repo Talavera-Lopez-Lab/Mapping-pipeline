0b739109873b79a624d2047371958c28b675460c
cartal <carlos.talavera-lopez@uni-wuerzburg.de>
2023-10-12 13:44:05 +0200

    Added modified mapping script

--- mapping_samples_with_STAR.sh
+++ mapping_samples_with_STAR.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-### Set up working environment
-
-CPU=16
-STAR_INDEX=/home/amaguza/data/human_reference_genome/index_file_STAR
-base_url="ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR114"
-BASE_DIR="/home/amaguza/data/test_heart_data/GEX"
-STAR_CMD="/home/amaguza/STAR-2.7.11a/source/STAR"
-WL=/home/amaguza/data/10X_Genomics_whitelists/737K-arc-v1.txt
-
 # Declare an associative array with run accessions as keys and sample names as values
 declare -A samples
 samples=(
@@ -18,8 +9,17 @@ samples=(
   #... add other samples as needed
 )
 
+# Base URL
+base_url="ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR114"
+
 # File suffixes
 suffixes=("S1_L001_I2_001.fastq.gz" "S1_L001_I1_001.fastq.gz" "S1_L001_R1_001.fastq.gz" "S1_L001_R2_001.fastq.gz")
+
+# Base directory
+BASE_DIR="/home/amaguza/data/test_heart_data/GEX"
+
+# Mapping command
+STAR_CMD="/home/amaguza/STAR-2.7.11a/source/STAR"
 
 # Iterate over the associative array
 for accession in "${!samples[@]}"; do
@@ -37,19 +37,18 @@ for accession in "${!samples[@]}"; do
     file_url="${base_url}/${accession}/${sample}_${suffix}"
     
     # Use axel to download the file with 40 connections
-    axel -n $CPU --output="${sample}_${suffix}" "$file_url"
+    axel -n 10 --output="${sample}_${suffix}" "$file_url"
     
     # Unzip the files
     gunzip "${sample}_${suffix}"
-  
   done
   
   # Mapping the fastq files to the genome
-  $STAR_CMD --runThreadN $CPU \
-            --genomeDir $STAR_INDEX \
+  $STAR_CMD --runThreadN 40 \
+            --genomeDir /home/amaguza/data/human_reference_genome/index_file_STAR \
             --readFilesIn "$sample_name_S1_L001_R1_001.fastq" "$sample_name_S1_L001_R2_001.fastq" \
             --runDirPerm All_RWX GZIP --soloBarcodeMate 1 --clip5pNbases 28 0 --soloType CB_UMI_Simple \
-            --soloCBwhitelist $WL \
+            --soloCBwhitelist /home/amaguza/data/10X_Genomics_whitelists/737K-arc-v1.txt \
             --soloCBstart 1 --soloCBlen 16 --soloUMIstart 17 --soloUMIlen 12 --soloStrand Forward \
             --soloUMIdedup 1MM_CR --soloCBmatchWLtype 1MM_multi_Nbase_pseudocounts --soloUMIfiltering MultiGeneUMI_CR \
             --soloCellFilter EmptyDrops_CR --outFilterScoreMin 30 \
