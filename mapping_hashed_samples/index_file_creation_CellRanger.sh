d22f863caab8a190bb0791c4620221c8461aa167
cartal <carlos.talavera-lopez@uni-wuerzburg.de>
2024-02-26 20:41:55 +0100

    Added example for mapping - ct5

---
+++ mapping_hashed_samples/index_file_creation_CellRanger.sh
@@ -0,0 +1,6 @@
+/home/amaguza/STAR-2.7.11a/source/STAR --runThreadN 40 --runMode genomeGenerate \
+--genomeDir /home/amaguza/data/human_reference_genome/index_file_STAR \
+--genomeFastaFiles /home/amaguza/data/human_reference_genome/GRCh38.p14.genome.fa \
+--sjdbGTFfile /home/amaguza/data/human_reference_genome/gencode.v44.chr_patch_hapl_scaff.annotation.gtf \
+/home/amaguza/data/human_reference_genome/gencode.v44.long_noncoding_RNAs.gtf \
+/home/amaguza/data/human_reference_genome/gencode.v44.tRNAs.gtf
